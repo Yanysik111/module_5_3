14157c5b83bd55017c0435f1de059ad7445e6e32
Yanysik111 <yanyisik@yandex.ru>
2024-06-22 19:05:24 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -1,13 +1,15 @@
 
 class Building:
-    def __init__(self, name, numberOfFloors, buildingType ):
-        self.name = name
+    def __init__(self, numberOfFloors, buildingType ):
         self.numberOfFloors = numberOfFloors
         self.buildingType = buildingType
+
+    def __str__(self):
+        return f'{self.buildingType}'
 
     def __eq__(self, other):
         return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
-h1 = Building('ЖК Горский', 18, 'кирпичное' )
-h2 = Building('Домик в деревне', 2, 'бревенчатое')
+h1 = Building( 18, 'кирпичное' )
+h2 = Building( 2, 'бревенчатое')
 print(h1 == h2)
